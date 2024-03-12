6794fd38232daaa323b86d5a223945a863cd7ba1
Aniket Chauhan <chauhananiket12801@gmail.com>
2024-03-12 23:39:18 +0530

    [feat] started desiging conuntry_picker in account's get started page

--- lib/features/account/screens/account_screen.dart
+++ lib/features/account/screens/account_screen.dart
@@ -32,6 +32,43 @@ class _AccountScreenState extends State<AccountScreen> {
                 ),
               ),
               const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  showCountryPicker(
+                    context: context,
+                    //Optional. Shows phone code before the country name.
+                    showPhoneCode: true,
+                    onSelect: (Country country) {
+                      print('Select country: ${country.flagEmoji}');
+                    },
+                    // Optional. Sets the theme for the country list picker.
+                    countryListTheme: CountryListThemeData(
+                      // Optional. Sets the border radius for the bottomsheet.
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        topRight: Radius.circular(20.0),
+                      ),
+                      // Optional. Styles the search field.
+                      inputDecoration: InputDecoration(
+                        labelText: 'Search',
+                        hintText: 'Start typing to search',
+                        prefixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: const Color(0xFF8C98A8).withOpacity(0.2),
+                          ),
+                        ),
+                      ),
+                      // Optional. Styles the text in the search field
+                      searchTextStyle: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                      ),
+                    ),
+                  );
+                },
+                child: const Text('Show country picker'),
+              ),
             ],
           ),
         ),
